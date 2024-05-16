f2883fe60a3813a9522bd618065afae262e6b40c
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-05-15 22:54:12 -0500

    Problema 5 - Graficos comparativos entre regiones usando lineas

---
+++ problema5/script5.js
@@ -0,0 +1,43 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  fetch('../data.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Hubo un error al cargar los datos.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      var chartData = new google.visualization.DataTable();
+      chartData.addColumn('string', 'Fecha');
+
+      data.forEach(item => {
+        chartData.addColumn('number', item.region);
+      });
+
+      var dates = [];
+      data[0].confirmed.forEach(item => {
+        dates.push(item.date);
+      });
+      chartData.addRows(dates.length);
+
+      data.forEach((region, index) => {
+        chartData.setColumnLabel(index + 1, region.region);
+        region.confirmed.forEach((item, i) => {
+          chartData.setValue(i, index + 1, parseInt(item.value));
+        });
+      });
+
+      var options = {
+        title: 'Comparativo de Confirmados por Región',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+      chart.draw(chartData, options);
+    })
+    .catch(error => console.error(error));
+}
